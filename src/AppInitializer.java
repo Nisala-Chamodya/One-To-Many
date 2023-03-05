2a63f5feb9a0d94455c8f24c159abff0f593423d
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2023-03-05 23:22:47 +0530

    One To Many step02

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -9,9 +9,13 @@ import java.util.ArrayList;
 public class AppInitializer {
     public static void main(String[] args) {
 
-        Owner owner1  = new Owner();
-        owner1.setoId("o001");
-        owner1.setName("Tharani");
+          Owner owner1  = new Owner();
+          owner1.setoId("o001");
+          owner1.setName("Tharani");
+
+        Owner owner2  = new Owner();
+        owner2.setoId("o002");
+        owner2.setName("uchithma");
 
 
         Pet pet1 = new Pet();
@@ -26,6 +30,18 @@ public class AppInitializer {
         pet2.setOwner(owner1 );
 
 
+        Pet pet3 = new Pet();
+        pet3.setpId("p003");
+        pet3.setName("rabbit");
+        pet3.setOwner(owner2 );
+
+
+        Pet pet4 = new Pet();
+        pet4.setpId("p004");
+        pet4.setName("bird");
+        pet4.setOwner(owner2 );
+
+
         //1 st step
         ArrayList<Pet> petList = new ArrayList<>();
         petList.add(pet1);
@@ -33,13 +49,20 @@ public class AppInitializer {
 
         owner1.setPetList(petList);
 
+
+
+        //2nd step
+        owner2.getPetList().add(pet3);
+        owner2.getPetList().add(pet4);
+
+
         Session session = FactoryConfiguaration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
 
-        session.save(owner1);
-        session.save(pet1);
-        session.save(pet2);
+        session.save(owner2);
+        session.save(pet3);
+        session.save(pet4);
 
         transaction.commit();
         session.close();
